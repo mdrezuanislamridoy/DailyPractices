0ebf4b1fa1f8f0b25724b7ff668c6417032a0ad7
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2023-11-23 16:28:25 +0600

    string and array

---
+++ 23-11-23/src/Array/String_/s1.java
@@ -0,0 +1,15 @@
+package Array.String_;
+
+public class s1 {
+    public static void main(String[] args) {
+        // String is an object . we use it by creating object .. string need "" double
+        // quote to get string . string is a collection of character
+        String str = new String("Hello world");
+
+        System.out.println(str);
+
+        // short cut to represent string.. easy to use
+        String ssr = "Hello ssrr";
+        System.out.println(ssr);
+    }
+}
---
+++ 23-11-23/src/Array/objArr.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class objArr {
+    public static void main(String[] args) {
+        // different things cannot assign in a single array
+        // int[] a ={2.4,5,"hello",true};
+
+        // object array can store it
+
+        // Object class is the root/super_class of all class 
+
+        Object[] a = { 5, 66, 7.4, "ami", "asi" };
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
