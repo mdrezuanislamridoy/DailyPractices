bae89dea6692a73501a05b2a708e1eb9289708d9
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2023-11-28 21:32:18 +0600

    Vector

--- 28-11-23/src/Vectors/V1.java
+++ 28-11-23/src/Vectors/V1.java
@@ -1,7 +1,14 @@
 package Vectors;
 
+import java.util.Vector;
+
 public class V1 {
     public static void main(String[] args) {
-        
+        Vector<Object> v = new Vector<>();
+         v.addElement("44");
+         v.addElement("Hola");
+         v.addElement(55);
+
+         System.out.println(v);
     }
 }
