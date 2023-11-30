977d5debc84ff2687e4da41b03849505d0d10492
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2023-11-30 16:57:36 +0600

    TreeSet & Maps

--- 29-11-23/src/ShortedSets/SS1.java
+++ 29-11-23/src/ShortedSets/SS1.java
@@ -1,8 +1,29 @@
 package ShortedSets;
 
+import java.util.TreeSet;
 
 public class SS1 {
     public static void main(String[] args) {
-        
+
+        //ShortedSet, NavigableSet, TreeSet ... all stores in Tree set and by default they are shorted
+        // Heterogeneous objects are not allowed (For Sorting needs)
+        // Object Classes should be implementing Comparable Interface (StringBuffer is not supported)
+        //  null insertion is not allowed (Exception)
+
+        TreeSet ts= new TreeSet<>();
+
+        ts.add(9);
+        ts.add(44);
+        System.out.println(ts);
+
+        // Different data types not allowed
+
+        // ts.add("st");
+
+        // System.out.println(ts);
+
+        ts.add(9); // Duplicates are not allowed
+        System.out.println(ts);
+         
     }
 }
