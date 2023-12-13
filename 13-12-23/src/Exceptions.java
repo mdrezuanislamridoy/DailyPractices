a359a0c9b5f3399ae2af20497f73dde9edf306c9
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2023-12-13 17:18:54 +0600

    exception

---
+++ 13-12-23/src/Exceptions.java
@@ -0,0 +1,24 @@
+public class Exceptions {
+    public static void main(String[] args)  {
+        try {
+            ee(4, 2);
+        } catch (MyException e) {
+            System.out.println("exception is: "+ e.getMessage());
+        }
+        
+    }
+
+    static void ee(int a , int b) throws MyException{
+        if (a+b<11) {
+            throw new MyException("Ghumao");
+        } else {
+            
+        }
+    }
+}
+
+class MyException extends Exception {
+    MyException(String e) {
+        super(e);
+    }
+}
